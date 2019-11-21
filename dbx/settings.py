57354536805bcbf8aea71e22f01843492e826b31
Van Der Fran <vander.fran@shrss.com>
2019-11-21 17:04:17 -0500

    Creating base.html

--- dbx/settings.py
+++ dbx/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'server.apps.ServerConfig',
 
     # 'view_breadcrumbs',
-  
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
--- server/models.py
+++ server/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+
 from django.contrib.auth.models import User
+
 
 class Domain(models.Model):
     name = models.CharField(verbose_name='Name', max_length=20)
@@ -11,7 +13,6 @@ class Domain(models.Model):
 
     def __str__(self):
         return self.name
-
 
 
 class Property(models.Model):
